2427cb33e169fc1b7cd1f1a4db5766968158730b
MatheusTurra <matheusfr.318@gmail.com>
2024-10-05 15:16:16 -0300

    Ano atual dinamico no rodape

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
@@ -7,35 +8,24 @@ using System.Diagnostics;
 namespace PasswordManager.ViewModels;
 
 public partial class MainWindowViewModel : ObservableValidator
-{   
+{
     [ObservableProperty]
-    [Required(ErrorMessage = "Mensagem validacao do nome")]
-    [MinLength(2, ErrorMessage = "Validacao tamanho minimo do nome do programa")]
+    [Required(ErrorMessage = "Digite o nome do programa ou site")]
+    [MinLength(2, ErrorMessage = "O nome do site ou programa deve conter mais de dois caracteres")]
     private string? _name = "";
 
-    [Required(ErrorMessage = "Mensagem validacao senha")]
-    [MinLength(5, ErrorMessage = "Validacao tamanho minimo da senha")]
+    [Required(ErrorMessage = "Digite o nome da senha")]
+    [MinLength(5, ErrorMessage = "A senha deve conter mais de cinco caracteres")]
     [ObservableProperty]
     private string? _password = "";
+
+    public string CurrentYear { get => DateTime.Now.Year.ToString(); }
+
 
     [RelayCommand]
     private void savePassword()
     {
         ValidateAllProperties();
-
-         //IEnumerable<ValidationResult> validationErrors = GetErrors();
-        
-        foreach (ValidationResult validation in GetErrors())
-        {
-            IEnumerable<string> erroList = validation.MemberNames;
-            
-        }
-
-        if (HasErrors)
-        {
-            return;
-        }
-
         getPasswordViewModel().createPassword(Name, Password);
     }
 
