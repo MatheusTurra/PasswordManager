30cdc50cefbf5a257cf666c1bd04eb7dd87a5a02
MatheusTurra <matheusfr.318@gmail.com>
2024-11-09 12:45:41 -0300

    Padrao mvvm refeito

---
+++ App/Services/FileService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services
+{
+    class FileService
+    {
+        public void createNewFile(string filePath, string fileContent)
+        {
+            using (FileStream file = File.Create(filePath))
+            {
+                byte[] writableContent = new UTF8Encoding(true).GetBytes(fileContent);
+                file.Write(writableContent);
+            }
+        }
+
+        public void createDirectory(string DirectoryPath)
+        {
+            if (!Directory.Exists(DirectoryPath))
+            {
+                Directory.CreateDirectory(DirectoryPath);
+            }
+        }
+
+        public string getProjectRootDirectory()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string? projectRootDirectory = Directory.GetParent(workingDirectory)?.Parent?.Parent?.FullName;
+
+            if (projectRootDirectory == null)
+            {
+                throw new Exception();
+            }
+
+            return projectRootDirectory;
+        }
+    }
+
+}
---
+++ App/ViewModels/MainViewModel.cs
@@ -0,0 +1,89 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using App.Services;
+using App.Models;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
+
+namespace App.ViewModels
+{
+    partial class MainViewModel: ObservableValidator
+    {
+        [Required(ErrorMessage = "Digite o nome do programa ou site")]
+        [MinLength(2, ErrorMessage = "O nome do site ou programa deve conter mais de dois caracteres")]
+        [ObservableProperty]
+        private string? _name = "";
+
+        [Required(ErrorMessage = "Digite o nome de usuário do programa ou site")]
+        [MinLength(3, ErrorMessage = "O nome de usuário do site ou programa deve conter mais de três caracteres")]
+        [ObservableProperty]
+        private string? _user = "";
+
+        [Required(ErrorMessage = "Digite o nome da senha")]
+        [MinLength(5, ErrorMessage = "A senha deve conter mais de cinco caracteres")]
+        [ObservableProperty]
+        private string? _password = "";
+
+        [Required(ErrorMessage = "Digite novamente o nome da senha")]
+        [MinLength(5, ErrorMessage = "A senha deve conter mais de cinco caracteres")]
+        [ObservableProperty]
+        private string? _repeatPassword = "";
+
+        public string CurrentYear { get => DateTime.Now.Year.ToString(); }
+
+        [RelayCommand]
+        private void savePassword()
+        {
+            ValidateAllProperties();
+
+            string projectRootDirectory = getFileService().getProjectRootDirectory();
+            string passwordsFolder = projectRootDirectory + "/Assets/Passwords";
+
+            getFileService().createDirectory(passwordsFolder);
+
+            string fileName = RemoveSpecialCharacters(Name);
+            string passwordFilePath = System.IO.Path.Combine(passwordsFolder, fileName + ".pwd");
+
+            string jsonPassword = createPasswordAsJson();
+
+            getFileService().createNewFile(passwordFilePath, jsonPassword);
+
+            Debug.WriteLine(passwordsFolder);
+        }
+        
+        private Password createPassword()
+        {
+            return new Password()
+            {
+                name = Name,
+                user = User,
+                password = Password,
+                repeatPassword = RepeatPassword
+            };
+        }
+
+        private string createPasswordAsJson()
+        {
+            return JsonSerializer.Serialize(createPassword());
+        }
+
+        private string RemoveSpecialCharacters(string? str)
+        {
+            string result = "";
+
+            if (str != null)
+            {
+                result = Regex.Replace(str, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled).ToLower();
+            }
+
+            return result;
+        }
+
+        protected FileService getFileService()
+        {
+            return new FileService();
+        }
+    }
+}
