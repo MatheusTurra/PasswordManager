e3a7a7eaccb7f5bee430d383f22d047dfe68ac74
MatheusTurra <matheusfr.318@gmail.com>
2024-12-01 15:29:21 -0300

    Testes: criacao do diretorio de senhas

--- Tests/Unit/ViewModels/MainPageViewModelTests.cs
+++ Tests/Unit/ViewModels/MainPageViewModelTests.cs
@@ -1,17 +1,34 @@
-﻿using App.ViewModels;
+﻿using App.Services;
+using App.ViewModels;
+using Moq;
 
 namespace Tests.Unit.ViewModels
 {
     [TestClass]
     public class MainPageViewModelTests
     {
-        [TestMethod]
-        public void isMainPageViewModelAccessible()
-        {
-            var mainPage = new MainPageViewModel();
 
-            string test = mainPage.unitTest();
-            Assert.AreEqual(test, "TODO: Unit tests");
+
+        //TODO: CRIAR NOVOS MOCKS AO EXECUTAR CADA METODO (PESQUISAR DECORATOR MSTESTS)
+        [TestMethod]
+        public void Test_SavePassword_CreatesPasswordDirectory()
+        {
+            //ARRANGE
+            string projectRootDirectory = "C://ProjectRoot/";
+
+            Mock<IFileService> fileService = new Mock<IFileService>();
+            fileService.Setup(service => service.getProjectRootDirectory()).Returns(projectRootDirectory);
+
+            //REFATORACAO: CRIAR METODO FACTORY PARA INSTANCIAR O METODO SOB TESTE
+            var mainPage = new MainPageViewModel(fileService.Object);
+
+            //ACT
+            mainPage.CreatePasswordFile();
+
+
+            //ASSERT
+            string expectedDirectoryPath = projectRootDirectory + "/Assets/Passwords";
+            fileService.Verify(service => service.createDirectory(expectedDirectoryPath), Times.Once());
         }
     }
 }
