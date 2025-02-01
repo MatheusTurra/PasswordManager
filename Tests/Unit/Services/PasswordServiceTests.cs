66f94d4e466eba0fdb917f63d8fb651a29e2002b
Matheus Turra <matheusfr.318@gmail.com>
2025-02-01 19:12:51 -0300

    Refatoracao: PasswordServiceTests

---
+++ Tests/Unit/Services/PasswordServiceTests.cs
@@ -0,0 +1,111 @@
+﻿using App.Models;
+using App.Services;
+using App.Services.Interfaces;
+using Moq;
+using Newtonsoft.Json;
+
+namespace Tests.Unit.Services
+{
+    [TestClass]
+    public class PasswordServiceTests
+    {
+        [TestMethod]
+        public void SavePassword_CreatesPasswordDirectory()
+        {
+            //ARRANGE
+            string projectRootDirectory = "C://ProjectRoot/";
+            Mock<IFileService> fileService = new Mock<IFileService>();
+            fileService.Setup(service => service.GetProjectRootDirectory()).Returns(projectRootDirectory);
+
+            PasswordService passwordService = new PasswordService(fileService.Object);
+            Password password = getFakePassword();
+            
+            //ACT
+            passwordService.CreatePasswordFile(password);
+
+            //ASSERT
+            string expectedDirectoryPath = projectRootDirectory + "/Assets/Passwords";
+            fileService.Verify(service => service.CreateDirectory(expectedDirectoryPath), Times.Once());
+        }
+
+        [TestMethod]
+        public void SavePassword_FileIsCreatedWithoutSpecialCharacters()
+        {
+            //ARRANGE
+            var fileService = new Mock<IFileService>();
+
+            string createNewFilePathParameter = "";
+            fileService.Setup(fs => fs.CreateNewFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Callback<string, string>((filePath, fileContent) => createNewFilePathParameter = filePath);
+
+            PasswordService passwordService = new PasswordService(fileService.Object);
+            Password password = getFakePassword();
+            password.name = "unitTéstSpecíãlCharactêrs!";
+
+            //ACT
+            passwordService.CreatePasswordFile(password);
+
+            //ASSERT
+            string fileName = Path.GetFileNameWithoutExtension(createNewFilePathParameter);
+            bool nameContainsOnlyAlphanumeric = fileName.All(name => Char.IsLetterOrDigit(name));
+            Assert.IsTrue(nameContainsOnlyAlphanumeric);
+        }
+
+        [TestMethod]
+        public void SavePassword_FileIsCreatedWithCorrectExtension()
+        {
+            //ARRANGE
+            var fileService = new Mock<IFileService>();
+
+            string createNewFilePathParameter = "";
+            fileService.Setup(fs => fs.CreateNewFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Callback<string, string>((filePath, fileContent) => createNewFilePathParameter = filePath);
+
+            //ACT
+            PasswordService passwordService = new PasswordService(fileService.Object);
+            Password password = getFakePassword();
+            password.name = "unitTestFileExtension";
+
+            passwordService.CreatePasswordFile(password);
+
+            //ASSERT
+            string fileName = Path.GetExtension(createNewFilePathParameter);
+            Assert.AreEqual<string>(fileName, ".pwd");
+        }
+
+        [TestMethod]
+        public void SavePassword_JsonFileCreatedWithFormData()
+        {
+            //ARRANGE
+            var fileService = new Mock<IFileService>();
+
+            string createNewFileJsonPasswordParameter = "";
+            fileService.Setup(fs => fs.CreateNewFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Callback<string, string>((filePath, fileContent) => createNewFileJsonPasswordParameter = fileContent);
+
+            PasswordService passwordService = new PasswordService(fileService.Object);
+            
+            Password password = getFakePassword();
+
+            //ACT
+            passwordService.CreatePasswordFile(password);
+
+            var jsonPassword = JsonConvert.DeserializeObject<Password>(createNewFileJsonPasswordParameter);
+            Assert.AreEqual<string>(password.name, jsonPassword?.name);
+            Assert.AreEqual<string>(password.user, jsonPassword?.user);
+            Assert.AreEqual<string>(password.password, jsonPassword?.password);
+            Assert.AreEqual<string>(password.repeatPassword, jsonPassword?.repeatPassword);
+
+        }
+
+        private Password getFakePassword()
+        {
+            return new Password() {
+                name = "TestPassword",
+                user = "user@test.com",
+                password = "12345",
+                repeatPassword = "12345"
+            };
+        }
+    }
+}
--- Tests/Unit/ViewModels/MainPageViewModelTests.cs
+++ Tests/Unit/ViewModels/MainPageViewModelTests.cs
@@ -10,96 +10,17 @@ namespace Tests.Unit.ViewModels
     [TestClass]
     public class MainPageViewModelTests
     {
+        /*
+
+
+
+        
+
+
 
 
         [TestMethod]
-        public void SavePassword_CreatesPasswordDirectory()
-        {
-            //ARRANGE
-            string projectRootDirectory = "C://ProjectRoot/";
-            Mock<IFileService> fileService = new Mock<IFileService>();
-            fileService.Setup(service => service.GetProjectRootDirectory()).Returns(projectRootDirectory);
-
-            var mainPage = new MainPageViewModel(fileService.Object);
-
-            //ACT
-            mainPage.CreatePasswordFile();
-
-            //ASSERT
-            string expectedDirectoryPath = projectRootDirectory + "/Assets/Passwords";
-            fileService.Verify(service => service.CreateDirectory(expectedDirectoryPath), Times.Once());
-        }
-
-        [TestMethod]
-        public void SavePassword_FileIsCreatedWithoutSpecialCharacters()
-        {
-            //ARRANGE
-            var fileService = new Mock<IFileService>();
-
-            string createNewFilePathParameter = "";
-            fileService.Setup(fs => fs.CreateNewFile(It.IsAny<string>(), It.IsAny<string>()))
-                .Callback<string, string>((filePath, fileContent) => createNewFilePathParameter = filePath);
-
-            var mainPage = new MainPageViewModel(fileService.Object);
-            mainPage.Name = "unitTéstSpecíãlCharactêrs!";
-
-            //ACT
-            mainPage.CreatePasswordFile();
-
-            //ASSERT
-            string fileName = Path.GetFileNameWithoutExtension(createNewFilePathParameter);
-            bool nameContainsOnlyAlphanumeric = fileName.All(name => Char.IsLetterOrDigit(name));
-            Assert.IsTrue(nameContainsOnlyAlphanumeric);
-        }
-
-        [TestMethod]
-        public void SavePassword_FileIsCreatedWithCorrectExtension()
-        {
-            //ARRANGE
-            var fileService = new Mock<IFileService>();
-            
-            string createNewFilePathParameter = "";
-            fileService.Setup(fs => fs.CreateNewFile(It.IsAny<string>(), It.IsAny<string>()))
-                .Callback<string, string>((filePath, fileContent) => createNewFilePathParameter = filePath);
-
-            //ACT
-            var mainPage = new MainPageViewModel(fileService.Object);
-            mainPage.Name = "unitTestFileExtension";
-
-            mainPage.CreatePasswordFile();
-
-            //ASSERT
-            string fileName = Path.GetExtension(createNewFilePathParameter);
-            Assert.AreEqual<string>(fileName, ".pwd");
-        }
-
-
-        [TestMethod]
-        public void SavePassword_JsonFileCreatedWithFormData()
-        {
-            //ARRANGE
-            var fileService = new Mock<IFileService>();
-            
-            string createNewFileJsonPasswordParameter = "";
-            fileService.Setup(fs => fs.CreateNewFile(It.IsAny<string>(), It.IsAny<string>()))
-                .Callback<string, string>((filePath, fileContent) => createNewFileJsonPasswordParameter = fileContent);
-
-            var mainPage = new MainPageViewModel(fileService.Object);
-            mainPage.Name = "SiteNameUnitTest";
-            mainPage.User = "userUnitTest";
-            mainPage.Password = "passwordUnitTest";
-            mainPage.RepeatPassword = "passwordUnitTest";
-
-
-            //ACT
-            mainPage.CreatePasswordFile();
-
-            var jsonPassword = JsonConvert.DeserializeObject<Password>(createNewFileJsonPasswordParameter);
-            Assert.AreEqual<string>(mainPage.Name, jsonPassword?.name);
-            Assert.AreEqual<string>(mainPage.User, jsonPassword?.user);
-            Assert.AreEqual<string>(mainPage.Password, jsonPassword?.password);
-            Assert.AreEqual<string>(mainPage.RepeatPassword, jsonPassword?.repeatPassword);
-
-        }
+        
+        */
     }
 }
